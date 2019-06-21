65a3c6a105fff9d489bde4acc42f9f1dff18a678
Dobry_kot <dputilin@moysklad.ru>
2019-06-22 01:44:51 +0300

    add info abaut my vars

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from classes.artifactory import user*
 import os, yaml
+
+from classes.artifactory import artifactory
+
 
 if __name__ == "__main__":
     
@@ -15,11 +17,17 @@ if __name__ == "__main__":
     artifactory_basic_url       = artifactory_conf['artifactory_url']
 
     """gropePolicy = ['read', 'writhe']"""
-    username    = ''
-    email       = ''
-    password    = ''
-    gropePolicy = ['']
-
+    usernames_delete    = ['ttestovich' , 'ttestov'] # for delete user(s) variable is list
+    gropePolicy         = ['readers'    , 'writers']   
+    username_create     = 'ttestovich'               # for create user variable is str
+    email               = 'ttestovich@mail.ru'
+    password            = 't4too7a'
+    
+    # init artifactory
     artifactory = artifactory()
+    # authorization in artifactory
     artifactory.auth(artifactory_basic_url, auth=(artifactory_admin_user, artifactory_admin_password))
-    artifactory.user_create(username = username, email = email, password = password)
+    # create new user
+    artifactory.user_create(username = username_create, email = email, password = password, gropePolicy = gropePolicy)
+    # delete old user(s)
+    # artifactory.user_delete(usernames_delete)
