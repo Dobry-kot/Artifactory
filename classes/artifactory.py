fc0d028e4915609a1e78f6f04c415459d8840445
Dobry_kot <dputilin@moysklad.ru>
2019-06-22 01:44:28 +0300

    update class. add default options

--- classes/artifactory.py
+++ classes/artifactory.py
@@ -1,19 +1,24 @@
 import requests
 import json
+import copy
 
 class artifactory():
-    
+    DEFAULTS_OPTIONS = { 
+                         'headers' :  {
+                                        'Content-Type' : 'application/json;charset=UTF-8'
+                                       }
+                         }   
+
     def __init__(self):
-        pass
+        self._options = copy.copy(artifactory.DEFAULTS_OPTIONS)
         
     def auth(self, url, **auth_data):
-        self._session    = requests.session()
+        self._session   = requests.session()
         self.url        = url
         auth_url        = self.url + '/artifactory/ui/auth/login'
    
-        headers = {
-                    'Content-Type': 'application/json',
-                    } 
+        self._options['headers']['Content-Type'] = 'application/json'
+
         try:
             payload = {
                         "user"      :"%s" % auth_data['auth'][0],
@@ -24,45 +29,61 @@ class artifactory():
         except KeyError as error:
             pass
                     
-        auth = self._session.post(auth_url, headers=headers, data=json.dumps(payload)) 
-        print(auth.json())
+        auth = self._session.post(auth_url, 
+                                  headers = self._options['headers'], 
+                                  data    = json.dumps(payload)) 
 
-    def user_delete(self, username):
+        if auth.status_code == 200:
+            return True
 
+        else:
+            print(auth.json())
+
+    def user_delete(self, *usernames):
+        
         user_delete_url = self.url + '/artifactory/ui/users/userDelete'
 
-        headers = {
-                    'Content-Type': 'application/json;charset=UTF-8',
-                    }
-        payload = {
-                    "userNames":["%s" % username]
-                    }
-
-        user_delete = self._session.post(user_delete_url, headers=headers, data=json.dumps(payload)) 
-        print(user_delete.json())
-
-    def user_create(self, **account_data, *policy_data):
-
-        user_create_url = self.url + '/artifactory/ui/users'
-        headers = {
-                    'Content-Type': 'application/json;charset=UTF-8',
-                    }
+        self._options['headers']['Content-Type'] = 'application/json;charset=UTF-8'
+        
         try:
-            payload  = {"profileUpdatable"  : 'true',
+            
+            payload = {
+                    "userNames": usernames[0]
+                    }
+
+            user_delete = self._session.post(user_delete_url, 
+                                             headers = self._options['headers'], 
+                                             data    = json.dumps(payload))
+            print(user_delete.json())
+            
+        except KeyError and TypeError as error:
+            print('Error:artifactory.user_delete <%s>' % error) 
+
+    def user_create(self, **account_data):
+        user_create_url = self.url + '/artifactory/ui/users'
+
+        try:
+            payload  = {"profileUpdatable"  : "true",
                         "name"              : "%s" % account_data['username'],
                         "email"             : "%s" % account_data['email'],
                         "password"          : "%s" % account_data['password'],
                         "retypePassword"    : "%s" % account_data['password'],
                         }
-            groupeNames = list() 
+            groupNames = list() 
+ 
+            for groupName in account_data['gropePolicy']:
+                groupNames.append({"groupName" : "%s", "realm" : "artifactory"} % groupName)
 
-            for groupName in policy_data:
-                groupeNames.append('{"groupName" : "%s", "realm" : "artifactory"}' % groupeNames)
+            payload['userGroups'] = groupNames
+            
+        except KeyError and TypeError as error:
+            print(error)
 
-            payload.update['userGroups'] = groupName
-
-        except KeyError as error:
-            pass
-
-        user_create = self._session.post(user_create_url, headers=headers, data=json.dumps(payload)) 
-        print(user_create.json())
+        user_create = self._session.post(user_create_url,
+                                         headers    =self._options['headers'], 
+                                         data       =json.dumps(payload)) 
+        if user_create.status_code == 200:
+            return True
+        
+        else:
+            print(user_create.json())
